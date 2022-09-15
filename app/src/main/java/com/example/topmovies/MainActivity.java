2ac5da6cb33b262ffc5dd334d51fc1213b11f17e
Gaper03 <gabrielperao@hotmail.com>
2022-09-15 01:14:24 -0300

    Added first functionalities
    Added functionalities to load images from the web, to open secondary screen by clicking on any image and added new widgets on activity_main.xml

--- app/src/main/java/com/example/topmovies/MainActivity.java
+++ app/src/main/java/com/example/topmovies/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.topmovies;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,30 +21,23 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final int nMovies = 4;
+    public final int nMovies = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClick(View view) {
-        AsyncChangeVisibilityTask task = new AsyncChangeVisibilityTask();
+        AsyncLoadBannersTask task = new AsyncLoadBannersTask();
         task.execute(nMovies);
     }
 
-    class AsyncChangeVisibilityTask extends AsyncTask<Integer, Integer, String>{
+    class AsyncLoadBannersTask extends AsyncTask<Integer, Integer, String>{
         @Override
         protected String doInBackground(Integer... integers) {
             int nMovieBanners = integers[0];
             for (int i = 0; i < nMovieBanners; i++) {
-                try {
-                    Thread.sleep(1000);
-                    publishProgress(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                publishProgress(i);
             }
             return "Finalizado";
         }
@@ -51,15 +45,32 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            int id = getResources().getIdentifier("imageView" + values[0], "id", getPackageName());
-            ImageView imageView = findViewById(id);
+
+            int id = getResources().getIdentifier("movieBannerView" + values[0], "id", getPackageName());
+            final ImageView imageView = findViewById(id);
             imageView.setVisibility(View.VISIBLE);
+
+            String url;
+            if (values[0] % 3 == 0) url = "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg";
+            else url = "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu";
+
+            Picasso.get().load(url).into(imageView, new Callback() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    imageView.setImageResource(R.drawable.default_movie_banner);
+                }
+            });
+
             // TODO: trocar url da image
             // TODO: usar o Picasso para pegar imagem direto do server
             imageView.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View view) {
-                    System.out.println("Clicou");
                     Intent i = new Intent(MainActivity.this, MovieDescActivity.class);
                     startActivity(i);
                 }
